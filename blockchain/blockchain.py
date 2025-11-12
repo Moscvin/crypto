19cdbfa85e4941366ab7c4b638c7f096c8be7dcc
Vlad <moscvin.2002@gmail.com>
2025-11-12 15:20:41 +0200

    Refactor blockchain validation logic and adjust hash length in dataToHash function

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -1,6 +1,6 @@
-import json  # Importăm modulul json
+import json  
 
-import hashlib  # Importăm modulul hashlib pentru funcții de hash
+import hashlib  
 
 blockchain = [
     {
@@ -13,7 +13,7 @@ blockchain = [
 def dataToHash(data):
     json_data = json.dumps(data, sort_keys=True)
     binary_data = json_data.encode()
-    return hashlib.sha256(binary_data).hexdigest()[:10] # Returnează primele 10 caractere ale hash-ului
+    return hashlib.sha256(binary_data).hexdigest()[:4] 
 
 def addNewBlock(from_address, to_address, amount):
     prev_block = blockchain[-1]
@@ -25,17 +25,38 @@ def addNewBlock(from_address, to_address, amount):
         "prev_hash": prev_hash
     }
     blockchain.append(new_block)
+
 def validateBlockchain():
     prev_block = None
     for block in blockchain:
-        if prev_block is not None:
-            expected_prev_hash = dataToHash(prev_block)
-            if block.get("prev_hash") != expected_prev_hash:
-                return False
+        if prev_block:
+            actual_prev_hash = dataToHash(prev_block)
+            recorded_prev_hash = block["prev_hash"] 
+            if actual_prev_hash != recorded_prev_hash:
+                print("Blockchain is invalid!, expected prev_hash:", actual_prev_hash, "but got:", recorded_prev_hash)
+            else:
+                print("Block is valid, prev_hash matches:", actual_prev_hash) 
         prev_block = block
-    return True
 
 addNewBlock("Alice", "Bob", 50.0)
 addNewBlock("Bob", "Charlie", 25.0)
 addNewBlock("Charlie", "Dave", 10.0)
 print(json.dumps(blockchain, indent=4))
+print("Validating blockchain...")
+validateBlockchain()
+
+block = 2 
+amount = blockchain[block]["amount"]
+expected_hash = blockchain[block+1]["prev_hash"]
+
+
+hash = ""
+ 
+while hash != expected_hash:
+    amount += 1
+    blockchain[block]["amount"] = amount
+    hash = dataToHash(blockchain[block])
+print("After adjusting amount to fix hash: , amount =", amount)
+print(json.dumps(blockchain, indent=4))
+validateBlockchain()
+
